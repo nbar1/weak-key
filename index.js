96c3bb590774d89c251faac842f6947b1e41afa9
nbar1 <xnickbarone@gmail.com>
2017-07-14 19:56:09 -0400

    Added ability to prefix the key

--- index.js
+++ index.js
@@ -2,13 +2,24 @@ var WeakMap = global.WeakMap || require('core-js/library/fn/weak-map');
 var map = new WeakMap();
 var index = 0;
 
-module.exports = weakKey;
+/**
+ * weakKey
+ *
+ * @param {object} obj
+ * @param {string} [prefix="weak-key"]
+ * @returns {string}
+ */
+function weakKey(obj, prefix) {
+	var key = map.get(obj);
 
-function weakKey(obj) {
-  var key = map.get(obj);
-  if (!key) {
-    key = 'weak-key-' + index++;
-    map.set(obj, key);  
-  }
-  return key;
+	prefix = (prefix) ? prefix : 'weak-key';
+
+	if (!key) {
+		key = prefix + '-' + index++;
+		map.set(obj, key);
+	}
+
+	return key;
 }
+
+module.exports = weakKey;
